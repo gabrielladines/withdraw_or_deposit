ac43a32545558e69ad9ef990cda8fa3657302cc1
gabrielladines <124473160+gabrielladines@users.noreply.github.com>
2025-01-01 00:33:06 +0000

    Update control_flow_atm.py

--- control_flow_atm.py
+++ control_flow_atm.py
@@ -1,21 +1,26 @@
 balance = 1000
 pin = 1234
-wrong_turns = 1
+pin_attempts = 1
 enter_pin = int(input("Eneter PIN: "))
+while enter_pin != pin:
+    enter_pin = int(input("Wrong PIN. Try again. "))
+    pin_attempts += 1
+    if pin_attempts == 3:
+        print("Card has been blocked due to too many wrong attempts.")   
+        break
 if enter_pin == pin:
-    print(balance)
+    print(f"Your balance is: £{balance}")
     withdraw_or_deposit = input("Would you like to withdraw or deposit? ").capitalize()
     if withdraw_or_deposit == "Withdraw":
         withdraw_amount = int(input("How much would you like to withdraw? "))
-        new_withdraw_balance = balance - withdraw_amount
-        print(f"Your new balance is £{new_withdraw_balance}")
+        if withdraw_amount > balance:
+            new_withdraw_amount = int(input(f"Not enough funds. Please select amount less than or equal to £{balance}. "))
+            balance -= new_withdraw_amount
+            print(f"Your new balance is £{balance}")
+        else:
+            balance -= withdraw_amount
+            print(f"Your new balance is £{balance}")    
     elif withdraw_or_deposit == "Deposit":
         deposit_amount = int(input("How much would you like to deposit? "))
         new_deposit_amount = balance + deposit_amount
         print(f"Your new balance is £{new_deposit_amount}")
-else:
-    int(input("Wrong PIN. Try again. "))
-while enter_pin != pin and wrong_turns < 2:
-    int(input("Wrong PIN. Try again. "))
-    wrong_turns += 1
-    print("Sorry. Card blocked due to wrong PIN too many times.")
